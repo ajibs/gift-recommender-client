b2d532379ef2fefeb2c0beee32e453d4b1dc89ac
Bolu Ajibawo <bolu.ajibawo@konga.com>
2019-03-10 16:32:50 +0100

    Setup wrapper around axios to easily swap out client if necessary

---
+++ src/libs/HttpRequestClient/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+class HttpRequestClient {
+    axiosInstance = null;
+    constructor () {
+      this.axiosInstance = axios.create({
+        baseURL: `${process.env.REACT_APP_HOST}`,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        transformResponse: [
+          (data) => this.handleResponse(data)
+        ]
+      });
+    }
+
+    handleResponse (data) {
+      let response;
+
+      try {
+        response = JSON.parse(data);
+      } catch (error) {
+        throw Error(`[HttpRequestClient] Error parsing response JSON data - ${JSON.stringify(error)}`);
+      }
+
+      if (response.status === 200 && response.data) {
+        return response.data;
+      } else {
+        throw new Error(response.message);
+      }
+    }
+
+    get (url, params = {}) {
+      return this.axiosInstance
+        .get(url, params);
+    }
+
+    post (url, params = {}) {
+      return this.axiosInstance
+        .post(url, params);
+    }
+
+    put (url, params = {}) {
+      return this.axiosInstance
+        .put(url, params);
+    }
+}
+
+export default new HttpRequestClient();
